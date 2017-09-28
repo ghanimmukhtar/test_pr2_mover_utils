659693c28d8d80a174d8627fc28abeb01b4f86fa
ghanimmukhtar <ghanimmukhtar@gmail.com>
2017-09-28 11:17:16 +0200

    good, smooth execution of the reversed trajectory

--- src/test_pr2_mover_utils.cpp
+++ src/test_pr2_mover_utils.cpp
@@ -1,83 +1,64 @@
 #include <pr2_mover_utils/pr2_mover.hpp>
-//#include <pr2_mover_utils/pr2_mover_utils/pr2_mover.hpp>
-//#include <moveit/move_group_interface/move_group.h>
+#include <moveit/trajectory_processing/iterative_time_parameterization.h>
+#include <moveit/robot_state/conversions.h>
 
 using namespace pr2_mover;
 
 int main(int argc, char **argv)
-{
-    ros::init(argc, argv, "test_pr2_utils");
-    ros::NodeHandle node;
-    PR2_Mover::Ptr _my_test;
-    _my_test.reset(new PR2_Mover(node));
-    moveit::planning_interface::MoveGroup::Plan _group_plan;
+    {
+        ros::init(argc, argv, "test_pr2_utils");
+        ros::NodeHandle node;
+        PR2_Mover::Ptr _my_test;
 
-    double x, y, z, xmin = 0.4, xmax = 0.8, ymin = 0.12, ymax = 1.0, zmin = 0.2, zmax = 1.3;
-    node.getParam("X", x);
-    node.getParam("Y", y);
-    node.getParam("Z", z);
+        _my_test.reset(new PR2_Mover(node));
+        moveit::planning_interface::MoveGroup::Plan _group_plan;
 
-//    ROS_WARN_STREAM("TEST: The planning time is: " << _my_test->group->getPlanningTime());
-//    ROS_WARN_STREAM("TEST: The eef pose is: ");
-//    ROS_WARN_STREAM("TEST: X: " << _my_test->group->getCurrentPose().pose.position.x);
-//    ROS_WARN_STREAM("TEST: Y: " << _my_test->group->getCurrentPose().pose.position.y);
-//    ROS_WARN_STREAM("TEST: Z: " << _my_test->group->getCurrentPose().pose.position.z);
-//    ROS_WARN_STREAM("TEST: RX: " << _my_test->group->getCurrentPose().pose.orientation.x);
-//    ROS_WARN_STREAM("TEST: RY: " << _my_test->group->getCurrentPose().pose.orientation.y);
-//    ROS_WARN_STREAM("TEST: RZ: " << _my_test->group->getCurrentPose().pose.orientation.z);
-//    ROS_WARN_STREAM("TEST: RW: " << _my_test->group->getCurrentPose().pose.orientation.w);
-    //_my_test->group->setPlannerId("RRTConnectkConfigDefault");
-   // _my_test->group->setPlanningTime(50);
+        robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
+        robot_model::RobotModelPtr robot_model = robot_model_loader.getModel();
 
-    _my_test->group->setPositionTarget(x, y, z);
-    if(_my_test->group->plan(_group_plan))
-        _my_test->group->execute(_group_plan);
 
-    while(ros::ok()){
-        moveit::planning_interface::MoveGroup::Plan _revers_plan;
-        x = (xmax - xmin) * ( (double)rand() / (double)RAND_MAX ) + xmin;
-        y = (ymax - ymin) * ( (double)rand() / (double)RAND_MAX ) + ymin;
-        z = (zmax - zmin) * ( (double)rand() / (double)RAND_MAX ) + zmin;
+        double x, y, z, xmin = 0.4, xmax = 0.8, ymin = 0.12, ymax = 1.0, zmin = 0.2, zmax = 1.3;
+        node.getParam("X", x);
+        node.getParam("Y", y);
+        node.getParam("Z", z);
 
-        ROS_INFO_STREAM("TEST : I am planning for position, X : " << x << ", Y : " << y << ", Z : " << z);
         _my_test->group->setPositionTarget(x, y, z);
         if(_my_test->group->plan(_group_plan))
             _my_test->group->execute(_group_plan);
 
-        //then try to execute it in reverse
-        //_revers_plan = _group_plan;
-        _revers_plan.trajectory_.joint_trajectory.header.stamp = ros::Time::now();
-        _revers_plan.trajectory_.joint_trajectory.header.frame_id = _group_plan.trajectory_.joint_trajectory.header.frame_id;
-        _revers_plan.trajectory_.joint_trajectory.joint_names = _group_plan.trajectory_.joint_trajectory.joint_names;
+        while(ros::ok()){
+                moveit::planning_interface::MoveGroup::Plan _revers_plan;
+                x = (xmax - xmin) * ( (double)rand() / (double)RAND_MAX ) + xmin;
+                y = (ymax - ymin) * ( (double)rand() / (double)RAND_MAX ) + ymin;
+                z = (zmax - zmin) * ( (double)rand() / (double)RAND_MAX ) + zmin;
 
-        int j = _group_plan.trajectory_.joint_trajectory.points.size() - 1;
-        for(size_t i = 0; i < _group_plan.trajectory_.joint_trajectory.points.size() && j >= 0; i++){
+                ROS_INFO_STREAM("TEST : I am planning for position, X : " << x << ", Y : " << y << ", Z : " << z);
+                _my_test->group->setPositionTarget(x, y, z);
+                if(_my_test->group->plan(_group_plan)){
+                        _my_test->group->execute(_group_plan);
+                        //then try to execute it in reverse
+                        _revers_plan.trajectory_.joint_trajectory.header.stamp = ros::Time::now();
+                        _revers_plan.trajectory_.joint_trajectory.header.frame_id = _group_plan.trajectory_.joint_trajectory.header.frame_id;
+                        _revers_plan.trajectory_.joint_trajectory.joint_names = _group_plan.trajectory_.joint_trajectory.joint_names;
 
-            trajectory_msgs::JointTrajectoryPoint pt;
-            pt.positions = _group_plan.trajectory_.joint_trajectory.points[j].positions;
-            pt.velocities = _group_plan.trajectory_.joint_trajectory.points[j].velocities;
-            pt.accelerations = _group_plan.trajectory_.joint_trajectory.points[i].accelerations;
-            pt.effort = _group_plan.trajectory_.joint_trajectory.points[j].effort;
-//            pt.velocities.resize(7, 0.0);
-//            pt.accelerations.resize(7, 0.0);
-//            pt.effort.resize(7, 0.0);
-            pt.time_from_start = _group_plan.trajectory_.joint_trajectory.points[i].time_from_start;
-            //_revers_plan.trajectory_.joint_trajectory.points[i] = _group_plan.trajectory_.joint_trajectory.points[j];
-            //_revers_plan.trajectory_.joint_trajectory.points[i].velocities.resize(7, 0);
-            //_revers_plan.trajectory_.joint_trajectory.points[i].accelerations.resize(7, 0);
-            //_revers_plan.trajectory_.joint_trajectory.points[i].effort.resize(7, 0);
-            //_revers_plan.trajectory_.joint_trajectory.points[i].time_from_start = _group_plan.trajectory_.joint_trajectory.points[i].time_from_start;
-            _revers_plan.trajectory_.joint_trajectory.points.push_back(pt);
-            j--;
-        }
+                        int j = _group_plan.trajectory_.joint_trajectory.points.size() - 1;
+                        trajectory_processing::IterativeParabolicTimeParameterization time_param;
+                        robot_trajectory::RobotTrajectory my_robot_trajectory(robot_model, _my_test->group->getName());
+                        robot_state::RobotState r_state_holder(robot_model);
+                        for(size_t i = 0; i < _group_plan.trajectory_.joint_trajectory.points.size() && j >= 0; i++){
+                                ROS_WARN_STREAM("TEST : In the for loop trying to construct the inverse trajectory with size : " << _group_plan.trajectory_.joint_trajectory.points.size());
+                                moveit::core::jointTrajPointToRobotState(_group_plan.trajectory_.joint_trajectory, j, r_state_holder);
+                                my_robot_trajectory.insertWayPoint(i, r_state_holder, 0.1);
+                                j--;
+                            }
 
-//        for(size_t i = 0; i < _my_test->group->getCurrentJointValues().size(); i++)
-//            ROS_INFO_STREAM("TESTER PR2 MOVER : Joint " << i << " value is :  " << _my_test->group->getCurrentJointValues()[i]);
-
-        _my_test->group->execute(_revers_plan);
-        std::cin.ignore();
+                        if(!time_param.computeTimeStamps(my_robot_trajectory))
+                            ROS_WARN("TEST : Time parametrization for the solution path failed.");
+                        my_robot_trajectory.getRobotTrajectoryMsg(_revers_plan.trajectory_);
+                        moveit::core::robotStateToRobotStateMsg(*_my_test->group->getCurrentState(), _revers_plan.start_state_);
+                        _my_test->group->execute(_revers_plan);
+                        std::cin.ignore();
+                    }
+            }
+        return 0;
     }
-
-    //ros::waitForShutdown();
-    return 0;
-}
